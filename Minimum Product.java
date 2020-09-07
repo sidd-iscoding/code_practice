955d71b051d6ccae57e04f27b3f5fbadcb3f97be
sidd-iscoding <51014778+sidd-iscoding@users.noreply.github.com>
2020-09-07 16:22:22 +0530

    Update Minimum Product.java

--- Minimum Product.java
+++ Minimum Product.java
@@ -1,5 +1,19 @@
 import java.util.Scanner;
 import java.lang.Math;
+
+/**@author Siddhartha
+Problem Statement: You are given four integers a, b, x and y. Initially, a≥x and b≥y. You can do the following operation no more than n times:
+
+Choose either a or b and decrease it by one. However, as a result of this operation, value of a cannot become less than x, and value of b cannot become less than y.
+Your task is to find the minimum possible product of a and b (a⋅b) you can achieve by applying the given operation no more than n times.
+
+You have to answer t independent test cases.
+
+References:
+Codeforces Round #667 (Div. 3)  1409B - Minimum Product
+
+**/
+
 public class mp{
 		long  a,b;
 		
@@ -26,7 +40,8 @@ public class mp{
 			objx.a=in.nextInt();
 			objx.b=in.nextInt();
 			n=in.nextInt();
-			
+			//highest product possible
+			//da for highest reduction posible for "a" while db is the highest reduction possible for "b" after reducing 'a'
 			pro=obj.a*obj.b;
 			for(int j=0;j<2;j++){
 				long da=Math.min(n,obj.a-objx.a);    
