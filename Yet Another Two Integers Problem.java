02e38cd5bcaed6c214db15ad49ba73a08a8df0ec
sidd-iscoding <51014778+sidd-iscoding@users.noreply.github.com>
2020-09-07 16:31:24 +0530

    Update Yet Another Two Integers Problem.java

--- Yet Another Two Integers Problem.java
+++ Yet Another Two Integers Problem.java
@@ -1,6 +1,23 @@
 import java.util.Scanner;
 import java.lang.Math;
 /**
+problem statement:You are given two integers a and b.
+
+In one move, you can choose some integer k from 1 to 10 and add it to a or subtract it from a. In other words, you choose an integer k∈[1;10] and perform a:=a+k or a:=a−k. You may use different values of k in different moves.
+
+Your task is to find the minimum number of moves required to obtain b from a.
+
+You have to answer t independent test cases.
+
+Input
+The first line of the input contains one integer t (1≤t≤2⋅104) — the number of test cases. Then t test cases follow.
+
+The only line of the test case contains two integers a and b (1≤a,b≤109).
+
+Output
+For each test case, print the answer: the minimum number of moves required to obtain b from a.
+
+References: Codeforces Round #667 (Div. 3)  1409A - Yet Another Two Integers Problem
 **/
 
 public class ytp{
